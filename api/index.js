1fec8d1c7a9f08633842aa2fc685cc719322eede
Neczx <lnwzaaaaaaa55@gmail.com>
2026-05-01 09:01:37 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
 print("ZeIoNhUb: โหลดสำเร็จ!")
 
 -- โหลดสคริปใหม่
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Teufkijdjd/Dextzr-build-a-boat-config/refs/heads/main/d8ad56c60f34318c.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Teufkijdjd/Dextzr-build-a-boat-config/refs/heads/main/a8b2096ce1e852e1.lua.txt"))()
       `);
     } else {
       res.status(404).send("404: NOT FOUND");
